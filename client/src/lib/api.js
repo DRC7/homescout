9158ac5507c2461b0d3d13760354633f40f64eb8
Daniel Cuzco <daniel.cuzco@gmail.com>
2026-03-09 13:28:08 -0400

    Use of apiFetch

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL;
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5050";
 
 export async function apiFetch(path, options = {}) {
   const res = await fetch(`${API_BASE}${path}`, {
